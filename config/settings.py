c59b9950a99ca01fc1f627d156ef3ea4903d63b7
Kutalov Konstantin <kutalov.k.v@mail.ru>
2024-05-13 22:34:45 +0700

    .

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,11 @@ CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
--- courses/tasks.py
+++ courses/tasks.py
@@ -1,6 +1,7 @@
 from config.celery import app
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.conf import settings
 
 
 @app.task
@@ -14,7 +15,7 @@ def send_course_update_email(user_email, course_title):
     """
     subject = f'Обновление курса "{course_title}"'
     message = render_to_string('courses/course_update_email.html', {'course_title': course_title})
-    from_email = 'skyprokutalov.k.v@mail.ru'
+    from_email = settings.EMAIL_HOST_USER
     recipient_list = [user_email]
     send_mail(subject, message, from_email, recipient_list, html_message=message)
 
